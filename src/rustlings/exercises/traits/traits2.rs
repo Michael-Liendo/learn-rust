bfbcb9d1bbd0dab97262a71379e39745887081f5
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-21 20:36:43 -0400

    feat: traits

--- src/rustlings/exercises/traits/traits2.rs
+++ src/rustlings/exercises/traits/traits2.rs
@@ -11,13 +11,19 @@
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(mut self) -> Vec<String> {
+        let mut new_vec = self;
+        new_vec.push(String::from("Bar"));
+        new_vec
+    }
+}
 
 #[cfg(test)]
 mod tests {
