b939e8b6db442a2e86dcb05220a52a642db188cc
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-21 22:31:32 -0400

    feat: password generator

---
+++ proyects/password-generator/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+use rand::Rng;
+
+fn main() {
+    let characters =
+        "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string();
+
+    let mut password_length = String::new();
+
+    println!("Enter a password length");
+
+    let _ = io::stdin().read_line(&mut password_length);
+
+    let mut password = String::new();
+
+    for _i in 0..password_length.as_str().trim().parse().unwrap() {
+        let number = rand::thread_rng().gen_range(0..characters.len());
+
+        let char = characters.chars().nth(number).unwrap();
+
+        password.push(char)
+    }
+
+    println!("{}", password)
+}
